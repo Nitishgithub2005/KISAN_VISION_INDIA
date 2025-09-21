0ece7f679e223ae8d8cc1ba7e9c6d9d6a83d8aa7
Nitish R Maladakar <121116567+Nitishgithub2005@users.noreply.github.com>
2025-09-21 12:14:31 +0530

    Update Chatbot.jsx

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -47,7 +47,7 @@ const Chatbot = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBp8m7FY-8chr9jFt8s0tpp8LoFL_795B0", {
+      const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=API_KEY", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
